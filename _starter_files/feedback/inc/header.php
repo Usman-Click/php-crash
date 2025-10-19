659e6d20800cb5c59a587c380fab38e593eebc75
Usman-Click <47116342+Usman-Click@users.noreply.github.com>
2025-10-19 10:21:57 +0100

    Includes

--- _starter_files/feedback/inc/header.php
+++ _starter_files/feedback/inc/header.php
@@ -36,34 +36,3 @@
 
 <main>
   <div class="container d-flex flex-column align-items-center">
-    <img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
-    <h2>Feedback</h2>
-    <p class="lead text-center">Leave feedback for Traversy Media</p>
-    <form action="" class="mt-4 w-75">
-      <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
-      </div>
-      <div class="mb-3">
-        <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
-      </div>
-      <div class="mb-3">
-        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
-      </div>
-    </form>
-    </div>
-</main>
-
-<footer class="text-center mt-5">
-  Copyright &copy; 2022
-</footer>
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-</html>
